d1682877c75a5b7c049feb7ee6bbed9647358aa6
江田 光義 <koda.mitsuyoshi@tecotec.co.jp>
2020-12-22 18:46:24 +0900

    add .zshenv

---
+++ .zshenv
@@ -0,0 +1,11 @@
+# 個人的に追加したパッケージを優先的に扱うため /usr/local/bin にパスを通す
+export PATH=/usr/local/bin:$PATH
+
+# デフォルトのエディターを emacs に設定
+export EDITOR=emacs
+
+# デフォルトの pager を less から lv に設定
+export PAGER='lv -c'
+
+# 文字コードの指定
+export LANG=ja_JP.UTF-8
--- init.zsh
+++ init.zsh
@@ -13,8 +13,11 @@ done
 
 SCRIPT_DIR=$(cd $(dirname $0); pwd)
 
+echo  'source ${SCRIPT_DIR}/.zshenv'  >> "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/zshenv;
 echo  'source ${SCRIPT_DIR}/.zprofile'  >> "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/zprofile;
 echo  'source ${SCRIPT_DIR}/.zshrc'  >> "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/zshrc;
-source "${ZDOTDIR:-$HOME}"/.zprofile
+
+#source "${ZDOTDIR:-$HOME}"/.zprofile
+source "${ZDOTDIR:-$HOME}"/.zshenv
 source "${ZDOTDIR:-$HOME}"/.zshrc
 
